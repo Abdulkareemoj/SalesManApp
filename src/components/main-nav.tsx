6ccec3852f83e3f4f924f4c05d532c44a4bb5974
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-09-20 10:26:16 +0100

    fixed navbar and added routing

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,28 +1,24 @@
-import { cn } from "@/lib/utils";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-export function MainNav({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLElement>) {
+export function MainNav() {
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      <ul>
-        <li className="text-sm font-medium transition-colors hover:text-primary">
-          <a href="#">Overview</a>
-        </li>
-        <li className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-          <a href="#">Customers</a>
-        </li>
-        <li className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-          <a href="#">Products</a>
-        </li>
-        <li className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-          <a href="#">Settings</a>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav>
+        <ul className="flex items-center space-x-4 lg:space-x-6">
+          <li className="text-sm font-medium transition-colors hover:text-primary">
+            <Link to="/">Overview</Link>
+          </li>
+          <li className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
+            <Link to="/">Customers</Link>
+          </li>
+          <li className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
+            <Link to="/">Products</Link>
+          </li>
+          <li className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
+            <Link to="/">Settings</Link>
+          </li>
+        </ul>
+      </nav>
+    </Router>
   );
 }
