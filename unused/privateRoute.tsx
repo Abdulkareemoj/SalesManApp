9d20934904a0db58e800a849df4567c69482432f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-14 17:06:09 +0100

    meh

---
+++ unused/privateRoute.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Route } from "react-router-dom";
+import { useAuth } from "../authContext";
+
+export function PrivateRoute({ children, ...rest }: any) {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      element={
+        isAuthenticated ? (
+          children
+        ) : (
+          <Navigate to="/login" state={{ from: rest.location }} replace />
+        )
+      }
+    />
+  );
+}
