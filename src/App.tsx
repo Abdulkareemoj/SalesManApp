6b6b1ed4285a3c4fb08aefc48a078e746194bbd0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-14 17:08:43 +0100

    chore: :art: reworked routing
    app actually starts with the login page now
    
    no logins yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,59 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Navigate,
-  Routes,
-} from "react-router-dom";
-import AuthenticationPage from "./auth";
-import DashboardPage from "./dashboard";
-import { PrivateRoute } from "./components/privateRoute";
-import { AuthProvider } from "./components/authContext";
-import ErrorBoundary from "./components/ErrorBoundary";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Navigate,
+//   Routes,
+// } from "react-router-dom";
+// import AuthenticationPage from "./auth";
+// import DashboardPage from "./dashboard";
+// import { PrivateRoute } from "./components/privateRoute";
+// import { AuthProvider } from "./authContext";
+// import ErrorBoundary from "./components/ErrorBoundary";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <ErrorBoundary fallback={{ fallback: <div>Something went wrong.</div> }}>
+//       <AuthProvider>
+//         <Router>
+//           <Routes>
+//             <Route path="/" element={""} />
+//             <Route
+//               path="/dashboard"
+//               element={
+//                 <PrivateRoute>
+//                   <DashboardPage />
+//                 </PrivateRoute>
+//               }
+//             />
+//             <Route path="/auth" element={<AuthenticationPage />} />
+//           </Routes>
+//         </Router>
+//       </AuthProvider>
+//     </ErrorBoundary>
+//   );
+// }
+
+import { Route, Routes } from "react-router-dom";
+// import Register from "./pages/Register";
+import Login from "./pages/Login";
+import AuthRoute from "./components/authRoute";
+import DashboardPage from "./pages/dashboard";
+
+const App = () => {
   return (
-    <ErrorBoundary fallback={{ fallback: <div>Something went wrong.</div> }}>
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={""} />
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <DashboardPage />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/auth" element={<AuthenticationPage />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
-    </ErrorBoundary>
+    <>
+      <main className="flex align-items-center justify-content-center">
+        <Routes>
+          <Route element={<AuthRoute />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Route>
+          {/* <Route path="/register" element={<Register />} /> */}
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+    </>
   );
-}
+};
+
+export default App;
