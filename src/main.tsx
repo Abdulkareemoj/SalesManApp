c15d8b7229a5ba6c9a40f940f7b67576c9269154
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-14 17:09:12 +0100

    add auth

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../src/styles/App.css";
+import AuthProvider from "./components/authProvider.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
