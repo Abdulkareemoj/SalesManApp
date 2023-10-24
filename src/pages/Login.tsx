be0aa7f72517abeda84b0304d2cdf3fc565cb295
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-24 22:35:17 +0100

    correct screen size

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -94,7 +94,7 @@ const Login = ({ onSubmit }: LoginProps) => {
           className="hidden dark:block"
         />
       </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Button
           type="submit"
           className={cn(
