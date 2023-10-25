95954077c35e2a24172b06da50f0d69b4d016eba
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-25 23:18:50 +0100

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,4 @@
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Navigate,
-//   Routes,
-// } from "react-router-dom";
-// import AuthenticationPage from "./auth";
-// import DashboardPage from "./dashboard";
-// import { PrivateRoute } from "./components/privateRoute";
-// import { AuthProvider } from "./authContext";
-// import ErrorBoundary from "./components/ErrorBoundary";
-
-// export default function App() {
-//   return (
-//     <ErrorBoundary fallback={{ fallback: <div>Something went wrong.</div> }}>
-//       <AuthProvider>
-//         <Router>
-//           <Routes>
-//             <Route path="/" element={""} />
-//             <Route
-//               path="/dashboard"
-//               element={
-//                 <PrivateRoute>
-//                   <DashboardPage />
-//                 </PrivateRoute>
-//               }
-//             />
-//             <Route path="/auth" element={<AuthenticationPage />} />
-//           </Routes>
-//         </Router>
-//       </AuthProvider>
-//     </ErrorBoundary>
-//   );
-// }
-
 import { Route, Routes } from "react-router-dom";
-// import Register from "./pages/Register";
 import Login from "./pages/Login";
 import AuthRoute from "./components/authRoute";
 import DashboardPage from "./pages/dashboard";
