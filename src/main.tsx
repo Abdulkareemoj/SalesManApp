94f4e1092ef309527909c86c73ae62810f3e1fe3
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-30 17:19:20 +0100

    removed unused components

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../src/styles/App.css";
-import AuthProvider from "./components/authProvider.tsx";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
