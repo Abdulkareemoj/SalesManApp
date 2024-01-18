80eb37f9bc86064ed4368ace1f24c113f27d83ae
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-01-18 16:25:23 +0100

    add other pages

---
+++ src/app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import { Search } from "../../../components/search";
+export default function CustomersPage() {
+  return (
+    <div className="m-6">
+      <div>
+        <Search placeholder="Search for a Customer" />
+      </div>
+    </div>
+  );
+}
