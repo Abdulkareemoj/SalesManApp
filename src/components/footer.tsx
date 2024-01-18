0614ce9083a7fc0e7b2294c042b4f86f742e317b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-01-18 16:25:46 +0100

    add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="font-bold">Company</div>
+      <div className="text-sm">(c) All Rights Reserved.</div>
+    </div>
+  );
+}
