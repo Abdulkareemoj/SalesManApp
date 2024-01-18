66b5870928f9fb3a357e745f5f70118a01f3f4af
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-01-18 17:57:20 +0100

    formatting

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,5 +1,10 @@
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
-};
-
-export default ProductsPage;
+import { Search } from "../../../components/search";
+export default function ProductsPage() {
+  return (
+    <div className="m-6">
+      <div>
+        <Search placeholder="Search for a Product" />
+      </div>
+    </div>
+  );
+}
