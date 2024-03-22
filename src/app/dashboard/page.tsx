b7a77de824dede217ff227e4df053fed80bbedc6
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-03-22 21:22:24 +0100

    fixed header

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,11 +21,6 @@ export default function DashboardPage() {
   return (
     // <SessionAuth>
     <div className="hidden flex-col md:flex">
-      <div className="border-b">
-        <div className="flex h-4  items-center px-4">
-          {/* <TeamSwitcher /> */}
-        </div>
-      </div>
       <div className="flex-1 space-y-4 p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
