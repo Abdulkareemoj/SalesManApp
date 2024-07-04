b2889eb982791548a992de4c4eec73e1bae6b0ee
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-04 13:55:42 +0100

    refactor: Update dashboard page and settings layout styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,7 +18,7 @@ const metadata: Metadata = {
 
 export default function DashboardPage() {
   return (
-    <div className="hidden flex-col md:flex">
+    <div className=" flex-col md:flex">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
